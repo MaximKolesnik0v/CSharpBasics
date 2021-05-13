db220c38e89391881090f4f2341c55e695cc7557
Максим Колесников <maskkrutoy_2015@mail.ru>
2021-05-13 13:11:49 +0300

    IntegerSwapper.cs added.

---
+++ IntegerSwapper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace IntegerSwap
+{
+    class IntegerSwapper
+    {
+        /*
+         4. Написать программу обмена значениями двух переменных типа int без использования вспомогательных методов.
+         а) с использованием третьей переменной;
+         б) *без использования третьей переменной.
+        Колесников Максим
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            //Вариант А
+            SwapWithBuffer(a, b);
+            //Вариант Б
+            //SwapWithoutBuffer(a, b);
+
+            Console.ReadKey();
+        }
+
+        static void SwapWithBuffer(int a, int b)
+        {
+            int c = b;
+            b = a;
+            a = c;
+
+            Console.WriteLine($"Значение a: {a}");
+            Console.WriteLine($"Значение b: {b}");
+        }
+
+        static void SwapWithoutBuffer(int a, int b)
+        {
+            b += a;
+            a = b - a;
+            b = b - a;
+
+            Console.WriteLine($"Значение a: {a}");
+            Console.WriteLine($"Значение b: {b}");
+        }
+    }
+}
